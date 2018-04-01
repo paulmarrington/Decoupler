89b5b8e1c78ae5caff7e41b1390c8bab916ec6c3
paulmarrington <paul@marrington.net>
2018-04-01 21:39:59 +1000

    Starting on analytics with API

---
+++ Assets/Askowl/Decoupler/Services/Analytics.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Decoupled {
+  using JetBrains.Annotations;
+
+  public sealed class Analytics : Service<Analytics> {
+    [UsedImplicitly]
+    public void Error(string message, string action, [NotNull] params object[] more) {
+      Event(name: "Error", action: action, result: message, more: more);
+    }
+
+    [UsedImplicitly]
+    public void Event(string name, string action, string result, [NotNull] params object[] more) {
+      Debug.Log("**** Event '" + name + "' -- action: " + action + ", result: " + result +
+                ", more: "     + More(more));
+    }
+
+    [NotNull, UsedImplicitly]
+    public string More([NotNull] params object[] list) {
+      return string.Join(separator: ",",
+                         value: System.Array.ConvertAll(array: list, converter: x => x.ToString()));
+    }
+  }
+}
