f7bd0a287e0e28955047da66dd10d2115aa6bee6
paulmarrington <paul@marrington.net>
2018-04-02 14:03:07 +1000

    get unity analytics written and tested

--- Assets/Askowl/Decoupler/Services/Analytics.cs
+++ Assets/Askowl/Decoupler/Services/Analytics.cs
@@ -1,16 +1,17 @@
-﻿using UnityEngine;
-
-namespace Decoupled {
+﻿namespace Decoupled {
+  using System;
   using JetBrains.Annotations;
+  using UnityEngine;
 
-  public sealed class Analytics : Service<Analytics> {
+  public class Analytics : Service<Analytics> {
     [UsedImplicitly]
-    public void Error(string message, string action, [NotNull] params object[] more) {
-      Event(name: "Error", action: action, result: message, more: more);
+    public virtual void Error(string name, string message, [NotNull] params object[] more) {
+      Event(name: name, action: "Error", result: message, more: more);
     }
 
     [UsedImplicitly]
-    public void Event(string name, string action, string result, [NotNull] params object[] more) {
+    public virtual void Event(string                    name, string action, string result,
+                              [NotNull] params object[] more) {
       Debug.Log("**** Event '" + name + "' -- action: " + action + ", result: " + result +
                 ", more: "     + More(more));
     }
@@ -18,7 +19,20 @@ namespace Decoupled {
     [NotNull, UsedImplicitly]
     public string More([NotNull] params object[] list) {
       return string.Join(separator: ",",
-                         value: System.Array.ConvertAll(array: list, converter: x => x.ToString()));
+                         value: Array.ConvertAll(array: list, converter: x => x.ToString()));
     }
+
+    public enum Genders {
+      Male,
+      Female,
+      Other,
+      Unknown
+    }
+
+    public virtual Genders Gender { get { return gender; } set { gender = value; } }
+    protected      Genders gender = Genders.Unknown;
+
+    public virtual int BirthYear { get { return birthYear; } set { birthYear = value; } }
+    protected      int birthYear;
   }
 }
