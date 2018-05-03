b249dd307354cc019ff1bac578fc72887aa841eb
paulmarrington <paul@marrington.net>
2018-05-03 22:03:11 +1000

    fix up some left-over bugs

--- Assets/Askowl/Decoupler/Examples/FirstDecouplerInterface.cs
+++ Assets/Askowl/Decoupler/Examples/FirstDecouplerInterface.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+﻿#if UNITY_EDITOR && AskowlDecoupler
 namespace Decoupled {
   /// <inheritdoc />
   /// <summary>
--- Assets/Askowl/Decoupler/Examples/FirstDecouplerService.cs
+++ Assets/Askowl/Decoupler/Examples/FirstDecouplerService.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+﻿#if UNITY_EDITOR && AskowlDecoupler
 using Decoupled;
 
 internal sealed class FirstDecouplerService : FirstDecouplerInterface {
