a41342d9fe5189b72c1744e066ba7af0c5025331
paulmarrington <paul@marrington.net>
2018-06-17 19:35:10 +1000

    17/06/2018

--- Assets/Askowl/Decoupler/Services/Gyro.cs
+++ Assets/Askowl/Decoupler/Services/Gyro.cs
@@ -2,25 +2,80 @@
 using System.Collections;
 using UnityEngine;
 
-namespace Decoupled {
-  /// <inheritdoc />
-  /// <summary>
-  /// Interface to a device gyroscope.
-  /// </summary>
-  [Serializable]
-  public class Gyro : Service<Gyro> {
+namespace Askowl {
+  [CreateAssetMenu(menuName = "Custom Assets/Gyroscope")]
+  public class Gyro : CustomAsset.OfType<Decoupled.Gyro> {
     [SerializeField, Tooltip("larger for more stability, smaller for faster following")]
     private float minimumChange = 0.01f;
+
+    [SerializeField] private float updateIntervalInSeconds = 1;
+
+    /// <summary>
+    ///   <para>Sets or retrieves gyroscope interval in seconds.</para>
+    /// </summary>
+    public virtual float UpdateInterval {
+      get { return updateIntervalInSeconds; }
+      set { PollingInterval = new WaitForSecondsRealtime(updateIntervalInSeconds = value); }
+    }
+
+    /// <summary>
+    /// Set if Gyro failed to initialise
+    /// </summary>
+    public bool Offline { get { return Value.Offline; } }
+
+    /// <summary>
+    ///   <para>Returns the attitude (ie, orientation in space) of the device.</para>
+    /// </summary>
+    public Quaternion Attitude { get { return Value.Attitude; } }
 
     /// <summary>
     /// Amount of time between gyroscope checks (in seconds
     /// </summary>
     protected WaitForSecondsRealtime PollingInterval;
 
+    protected virtual void OnEnable() {
+      base.OnEnable();
+      Value          = Decoupled.Gyro.Instance;
+      UpdateInterval = updateIntervalInSeconds;
+    }
+
+    /// <summary>
+    /// Coroutine that checks for changes to coordinates at set intervals. This will trigger an event for any who are listening.
+    /// </summary>
+    public IEnumerator StartPolling() {
+      while (!Offline) {
+        if (!Equals(Value)) Changed();
+
+        yield return PollingInterval;
+      }
+    }
+
+    /// <summary>
+    /// Start a coroutine to poll the gyroscope on the given MonoBehaviour.
+    /// </summary>
+    /// <param name="monoBehaviour">The MonoBehaviour that owns the polling coroutine</param>
+    public virtual void Start(MonoBehaviour monoBehaviour) {
+      if (!Offline) monoBehaviour.StartCoroutine(StartPolling());
+    }
+
+    protected override bool Equals(Decoupled.Gyro other) {
+      float change = Mathf.Abs(Quaternion.Dot(Value.Attitude, other.LastReading)) - 1;
+      return (change <= minimumChange);
+    }
+  }
+}
+
+namespace Decoupled {
+  /// <inheritdoc />
+  /// <summary>
+  /// Interface to a device gyroscope.
+  /// </summary>
+//  [Serializable]
+  public class Gyro : Service<Gyro> {
     /// <summary>
     /// Used to compare to see if we have a change to report
     /// </summary>
-    protected Quaternion LastReading;
+    public Quaternion LastReading;
 
     /// <inheritdoc />
     /// <summary>
@@ -31,35 +86,6 @@ namespace Decoupled {
 
       Enabled = true;
     }
-
-    /// <summary>
-    /// Coroutine that checks for changes to coordinates at set intervals. This will trigger an event for any who are listening.
-    /// </summary>
-    public IEnumerator StartPolling() {
-      while (!Offline) {
-        float change = Mathf.Abs(Quaternion.Dot(Attitude, LastReading)) - 1;
-
-        if (change > minimumChange) {
-          LastReading = Attitude;
-          Changed();
-        }
-
-        yield return PollingInterval;
-      }
-    }
-
-    /// <summary>
-    /// Start a coroutine to poll the GPS on the given MonoBehaviour.
-    /// </summary>
-    /// <param name="monoBehaviour">The MonoBehaviour that owns the polling coroutine</param>
-    public virtual void Start(MonoBehaviour monoBehaviour) {
-      if (!Offline) monoBehaviour.StartCoroutine(StartPolling());
-    }
-
-    /// <summary>
-    /// Called on change so we can act on it
-    /// </summary>
-    protected virtual void Changed() { }
 
     /// <summary>
     /// Set if Gyro failed to initialise
@@ -95,13 +121,5 @@ namespace Decoupled {
     ///   <para>Sets or retrieves the enabled status of this gyroscope.</para>
     /// </summary>
     public virtual bool Enabled { get; set; }
-
-    /// <summary>
-    ///   <para>Sets or retrieves gyroscope interval in seconds.</para>
-    /// </summary>
-    public virtual float UpdateInterval {
-      get { return 1; }
-      set { PollingInterval = new WaitForSecondsRealtime(value); }
-    }
   }
 }
