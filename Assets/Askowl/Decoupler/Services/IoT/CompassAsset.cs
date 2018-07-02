4e56b3610ea811cd5886d9571b38e25c2e7127bc
paulmarrington <paul@marrington.net>
2018-07-02 22:15:00 +1000

    Tetrad class code complete (for now)

--- Assets/Askowl/Decoupler/Services/IoT/CompassAsset.cs
+++ Assets/Askowl/Decoupler/Services/IoT/CompassAsset.cs
@@ -10,28 +10,58 @@ namespace CustomAsset.Mutable {
     /// <see cref="OfType{T}.Value"/>
     public CompassService Device { get { return Value; } set { Value = value; } }
 
-    private float      settleTime;
-    private bool       settled;
-    private Quaternion magneticHeading;
+    private float                    settleTime;
+    private bool                     settled;
+    private Quaternion               magneticHeading;
+    private Vector3                  rotateFrom, rotateTo;
+    private float                    lastUpdateTime;
+    private ExponentialMovingAverage ema;
 
     public bool Ready {
       get {
         if (settled) return true;
         if (Time.realtimeSinceStartup < settleTime) return false;
 
+        ema        = new ExponentialMovingAverage(16);
+        rotateFrom = new Vector3(0, Device.MagneticHeading, 0);
+        rotateTo   = new Vector3(0, Device.MagneticHeading, 0);
+        Calibrate();
+
         return (settled = true);
       }
     }
-//
-//    public Quaternion MagneticHeading {
-//      get {
-//        if (!Device.Equals(null)) {
-//          var newHeading = Quaternion.Euler(0, -Device.MagneticHeading, 0);
-//          magneticHeading = newHeading;
-//        }
-//        return magneticHeading;
-//      }
-//    }
+
+    /*
+     * Jose Rodríguez-Rosa and Jorge Martín-Gutiérrez / Procedia Computer Science 25 (2013) 436 – 442
+     * page 439:
+     *
+     * Another more efficient method is the exponential moving average...
+     *
+     * This method requires fewer operations and gives much better results when filtering noise from
+     * compass sensor input data. This formula is computed in constant time. We empirically determined
+     * the value of parameter=0.2 as an optimal setting for this method.
+     */
+    public void Calibrate() {
+      // Use the exponential moving average to help smooth out compass variations
+      rotateTo.y     = ema.Average(Device.MagneticHeading);
+      lastUpdateTime = Time.realtimeSinceStartup;
+    }
+
+    /*
+     * Jose Rodríguez-Rosa and Jorge Martín-Gutiérrez / Procedia Computer Science 25 (2013) 436 – 442
+     * page 439:
+     *
+     * The Sn value is the horizontal rotation (around the Y axis) in degrees. This value is converted
+     * to a quaternion and interpolated (using slerp as done before with the gyroscope stabilization method)
+     * with the previous value using the number of seconds since the last reading as interpolation step
+     * parameter (since this method is executed several times per second, this is a fractional value between 0 and 1).
+     */
+    public float MagneticHeading {
+      get {
+        float elapsedSeconds = Time.realtimeSinceStartup - lastUpdateTime;
+        return Vector3.Slerp(rotateFrom, rotateTo, elapsedSeconds).y;
+      }
+    }
 
     /// <inheritdoc />
     public override CompassService Initialise() {
--- Assets/Askowl/Decoupler/Services/IoT/GyroAsset.cs
+++ Assets/Askowl/Decoupler/Services/IoT/GyroAsset.cs
@@ -1,17 +1,21 @@
-﻿using Decoupled;
+﻿using Askowl;
+using Decoupled;
 using UnityEngine;
 
 namespace CustomAsset.Mutable {
   /// <inheritdoc cref="Decoupled.GyroService" />
   [CreateAssetMenu(menuName = "Custom Assets/Device/Gyroscope"), ValueName("Device")]
   public class GyroAsset : OfType<GyroService> {
+    [SerializeField] private float smoothing = 0.2f;
+
     public static GyroAsset Instance { get { return Instance<GyroAsset>(); } }
 
     /// <see cref="OfType{T}.Value"/>
     public GyroService Device { get { return Value; } set { Value = value; } }
 
-    private float settleTime;
-    private bool  settled;
+    private float  settleTime;
+    private bool   settled;
+    private Tetrad rotateFrom = new Tetrad(), rotateTo = new Tetrad();
 
     public bool Ready {
       get {
@@ -19,6 +23,7 @@ namespace CustomAsset.Mutable {
 
         if (Device.Attitude == Quaternion.identity) return false;
 
+        rotateFrom.Set(Device.Attitude);
         settleTime = Time.realtimeSinceStartup - settleTime;
         return (settled = true);
       }
@@ -26,14 +31,34 @@ namespace CustomAsset.Mutable {
 
     public float SecondsSettlingTime { get { return settleTime; } }
 
-    /// Gyro is right-handed while Unity is left-handed.
-    public Quaternion Attitude { get { return RightToLeftHanded(Input.gyro.attitude); } }
+    /*
+     * Jose Rodríguez-Rosa and Jorge Martín-Gutiérrez / Procedia Computer Science 25 (2013) 436 – 442
+     * page 438:
+     *
+     * However, we encountered some issues when using gyroscope input data since it is very device dependent:
+     * Sometimes gyroscope attitude readings are noisy, update slowly or require a quaternion correction if
+     * the device screen orientation is changed (e.g. from Portrait to Landscape)[1].
+     *
+     * This was addressed using a quaternion spherical linear interpolation (slerp) between the current
+     * camera attitude and the desired one (which might be multiplied by a second quaternion to correct the
+     * screen orientation if needed) using a step factor of 0.2. This value was determined empirically and
+     * effectively makes the interpolation to act as a low-pass filter, stabilizing the gyroscope reading
+     * while keeping enough responsiveness for a good user experience. The sampling rate used for this sensor
+     * was 15Hz (higher rates shortens device battery).
+     *
+     * In our tests, we found gyroscope on Apple iPhone smartphone to be very stable and accurate over time;
+     * on the other hand, Android it ́s very device dependent, but having the low-pass filter implemented as
+     * stated above, effectively fixes this problem on most devices.
+     */
+    public Tetrad Attitude {
+      get {
+        rotateTo.Set(Device.Attitude).RightToLeftHanded();
+        var rotation = Quaternion.Slerp(rotateFrom, rotateTo, smoothing);
+        rotateFrom.Set(rotateTo);
+        return rotateTo;
 
-    private Quaternion attitude = new Quaternion();
-
-    public Quaternion RightToLeftHanded(Quaternion gyroAttitude) {
-      attitude.Set(gyroAttitude.x, gyroAttitude.y, -gyroAttitude.z, -gyroAttitude.w);
-      return attitude;
+//        return rotation;
+      }
     }
 
     /// <inheritdoc />
